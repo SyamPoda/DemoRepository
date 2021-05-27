542450080ef4e644a281a612e723a00bcbfced03
Syam <syampoda@eurofins.com>
2021-05-27 09:54:52 +0530

    my first commit

---
+++ Account/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount.Account
+{
+    class BankAccount
+    {
+
+        static void Main(string[] args)
+        {   
+            var account = new CustomerAccount();
+            account.CustomerDetails("Syam1", 112121212);                       
+            Console.WriteLine($"Account {account.AccountNumber} was created for {account.Owner} with {account.Balance} ");
+            account.WithDraw(1900);
+            Console.WriteLine($"Balance amount after withdraw:  {account.Balance}");
+            account.Deposit(1000);
+            Console.WriteLine($"Total amount after Deposit:  {account.Balance}");
+        }
+    }
+}
---
+++ Account/CustomerAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount.Account
+{
+    public class CustomerAccount
+    {
+        public int AccountNumber { get; set; }
+        public string Owner { get; set; }      
+                
+        public void CustomerDetails(string name,int accountNumber)
+        {
+            this.Owner = name;            
+            this.AccountNumber = accountNumber;
+            accountNumber++;
+        }
+
+        private decimal balance = 1000;
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set { 
+
+                if (value > 0)
+                {
+                    this.balance = value;
+                }
+
+                else
+                {
+
+                    throw new ArgumentOutOfRangeException("Account balance is 0");
+                
+                }
+            }
+        }
+        public void WithDraw(decimal amount)
+        {
+            if (amount < balance)
+            {
+                balance = balance - amount;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Insufficent funds available in your account to withdraw");
+            }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance = balance + amount;
+            }
+            else 
+            {
+                throw new ArgumentOutOfRangeException("Negative value is not allowed");
+            }
+        }
+
+
+    }
+}
